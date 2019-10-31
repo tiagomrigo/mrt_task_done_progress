1d5de7b0d3610509fcad1f6df19a765bd61a3366
Tiago Magrini Rigo <tiagomrigo@gmail.com>
2019-10-31 13:14:39 -0300

    task

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Task Done Progress Bar 100%',
+    'version': '12.0.1.0.0',
+    'category': 'Addon',
+    'sequence': 1,
+    'summary': "Set task's progress to 100% when a project task is set Done",
+    'description': "Module which sets the progress bar to 100% when a project task is set as Done in kanban state.",
+    'author': 'Tiago Magrini Rigo',
+    'website': '',
+    'depends': ['base','project'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'qweb': [],
+}
---
+++ models/task.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2019 Mr_T - Tiago Magrini Rigo 
+
+from odoo import api, fields, models, _
+
+class Task(models.Model):
+    _inherit = "project.task"
+
+    kanban_state = fields.Selection(inverse='_inverse_state')
+
+    @api.multi
+    def _inverse_state(self):
+        for obj in self:
+            if obj.kanban_state == 'done':
+                #set progress 100%
+                obj.write({'progress': 100})
