9c5a5c57fb9bc9185fe345692a8532858e20f277
Tiago Magrini Rigo <tiagomrigo@gmail.com>
2019-11-02 17:52:05 -0300

    Update __manifest__.py

--- mrt_task_done_progress/__manifest__.py
+++ mrt_task_done_progress/__manifest__.py
@@ -11,7 +11,7 @@
     'depends': ['base','project', 'hr_timesheet'],
     'data': [
     ],
-    'images':['static/description/task_list.png'],
+    'images':['static/description/task_list_cover.png'],
     'installable': True,
     'auto_install': False,
     'application': False,
